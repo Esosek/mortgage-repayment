6915bfbc45c81a2953fb1ab99ded0fbeb867eb90
Esosek <67476790+Esosek@users.noreply.github.com>
2024-07-10 14:49:36 +0200

    Added Form header

--- components/Form.tsx
+++ components/Form.tsx
@@ -10,8 +10,21 @@ export default function Form() {
     e.preventDefault();
   }
 
+  function handleClearForm() {
+    console.log("Form cleared");
+  }
+
   return (
-    <form onSubmit={handleSubmit} className="p-8">
+    <form onSubmit={handleSubmit} className="px-8">
+      <div className="flex justify-between items-center my-8">
+        <h1 className="font-bold text-lg">Mortgage Calculator</h1>
+        <button
+          onClick={handleClearForm}
+          className="text-sm underline text-slate-700 hover:text-slate-900"
+        >
+          Clear All
+        </button>
+      </div>
       <SubmitButton>
         <Image src={iconCalculator} alt="Calculator icon" />
         Calculate Repayments
