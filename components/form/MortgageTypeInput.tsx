fbdc1d7e8a36ffb68784754a4c34169e4d9dafa7
Ales Zima <ales@cpex.cz>
2024-07-12 15:23:15 +0200

    Added MortgageTypeInput

--- components/form/MortgageTypeInput.tsx
+++ components/form/MortgageTypeInput.tsx
@@ -1,30 +1,53 @@
-type MortgageTypeInputProps = {};
+"use client";
 
-export default function MortgageTypeInput(props: MortgageTypeInputProps) {
+import { useState } from "react";
+
+export default function MortgageTypeInput() {
+  const [activeRadio, setActiveRadio] = useState<Number | null>();
+  const mortgageTypes = ["Repayment", "Interest Only"];
+
+  const handleRadioSelect = (index: number) => setActiveRadio(index);
+
   return (
-    <fieldset>
+    <fieldset className="space-y-2 my-2 font-bold">
       <legend className="text-sm text-slate-700">Mortgage Type</legend>
 
-      <div className="flex gap-4 border-[1px]">
-        <input
-          type="radio"
-          id="contactChoice1"
-          name="mortgage-type"
-          value="repayment"
-          className="accent-lime"
-        />
-        <label htmlFor="contactChoice1">Repayment</label>
-      </div>
-
-      <div className="flex gap-4 border-[1px]">
-        <input
-          type="radio"
-          id="contactChoice1"
-          name="mortgage-type"
-          value="interest-only"
-        />
-        <label htmlFor="contactChoice1">Interest Only</label>
-      </div>
+      {mortgageTypes.map((type, index) => (
+        <button
+          key={type}
+          type="button"
+          onClick={() => handleRadioSelect(index)}
+          className={`flex items-center gap-4 w-full border-[1px] py-2 px-4 rounded-md hover:border-lime ${
+            activeRadio === index
+              ? "border-lime bg-lime/15"
+              : "border-slate-500"
+          }`}
+        >
+          <input
+            type="radio"
+            id={`mortgage-type-${index + 1}`}
+            name="mortgage-type"
+            value={type.toLowerCase()}
+            className="sr-only"
+            defaultChecked={activeRadio === index}
+          />
+          <div
+            className={`flex items-center justify-center size-4 rounded-full border-2 ${
+              activeRadio === index ? "border-lime" : "border-slate-900"
+            }`}
+          >
+            {activeRadio === index && (
+              <div className="size-2 bg-lime rounded-full"></div>
+            )}
+          </div>
+          <label
+            htmlFor={`mortgage-type-${index + 1}`}
+            className="pointer-events-none"
+          >
+            {type}
+          </label>
+        </button>
+      ))}
     </fieldset>
   );
 }
